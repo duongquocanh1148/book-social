17619976d2df7e61098101adbfc5ae189c7c99e9
devteria <devteriadotcom@gmail.com>
2024-05-01 09:02:08 +0700

    add spring cloud, open feign dependency

--- identity-service/src/main/java/com/devteria/identity/dto/request/ProfileCreationRequest.java
+++ identity-service/src/main/java/com/devteria/identity/dto/request/ProfileCreationRequest.java
@@ -1,4 +1,4 @@
-package com.devteria.profile.dto.request;
+package com.devteria.identity.dto.request;
 
 import java.time.LocalDate;
 
@@ -6,14 +6,15 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProfileCreationRequest {
     String userId;
     String firstName;
     String lastName;
+    //    @DobConstraint(min = 10, message = "INVALID_DOB")
     LocalDate dob;
     String city;
 }
