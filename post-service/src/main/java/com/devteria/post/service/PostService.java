b41dcf8c7936c83ac30c9c71e2b4f48c0e9884a4
Armin1148 <duongquocanh1148@gmail.com>
2026-06-18 12:19:41 +0700

    Add Post Service

--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -1,13 +1,49 @@
 package com.devteria.post.service;
 
+import com.devteria.post.dto.request.PostRequest;
+import com.devteria.post.dto.response.PostResponse;
+import com.devteria.post.entity.Post;
+import com.devteria.post.mapper.PostMapper;
+import com.devteria.post.repository.PostRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PostService {
 
+    PostRepository postRepository;
+    PostMapper postMapper;
+
+    public PostResponse createPost(PostRequest postRequest){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Post post = Post.builder()
+                .content(postRequest.getContent())
+                .userId(authentication.getName())
+                .createdDate(Instant.now())
+                .modifiedDate(Instant.now())
+                .build();
+
+        postRepository.save(post);
+        return postMapper.toPostResponse(post);
+    }
+
+    public List<PostResponse> getMyPosts(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String userId = authentication.getName();
+
+        return postRepository.findAllByUserId(userId)
+                .stream()
+                .map(postMapper::toPostResponse).toList();
+    }
 }
